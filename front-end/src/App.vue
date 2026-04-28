f08ea3743eb0b7dc48122492c22af3cb7839ad84
rosannryy205 <khanhvmps39093@gmail.com>
2026-04-28 23:40:45 +0700

    thirt commit

---
+++ front-end/src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+</script>
+
+<template>
+  <div class="flex flex-col min-h-screen">
+    <!-- /* Header cố định */ -->
+    <Header />
+
+
+    <main class="flex-1">
+      <router-view></router-view>
+    </main>
+
+    
+    <!-- /* Footer cố định */ -->
+    <Footer />
+  </div>
+</template>
+
+<!-- <style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style> -->
---
+++ front-end/src/components/Header.vue
@@ -0,0 +1,10 @@
+<template>
+    <header class="bg-white shadow-md">
+        <div class="max-w-7xl mx-auto justify-between items-center flex px-6 py-2 min-h-20">
+            <!-- logon -->
+             <div>
+                <img src="/logo.png" alt="" class = "h-20 object-contain">
+             </div>
+        </div>
+    </header>
+</template>
